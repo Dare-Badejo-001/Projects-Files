91d5d0e582577601a1f7efe22e65d7fd70787bcc
Dare Badejo <ooa.badejo@gmail.com>
2024-03-06 20:58:24 -0500

    updated

--- DSandA/DoublyLinkedList/DoublyLL.py
+++ DSandA/DoublyLinkedList/DoublyLL.py
@@ -4,4 +4,41 @@ class Node:
         self.next = None 
         self.prev = None
 
-class 
+class DoublyLinkedList: 
+    def __init__(self, value): 
+        new_node = Node(value) 
+        self.head = new_node 
+        self.tail = new_node
+        self.length += 1 
+    
+    def print_list(self): 
+        temp = self.head
+        while temp: 
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value): 
+        new_node = Node(value)
+        if self.length == 0 : 
+            self.head = new_node 
+            self.tail = new_node 
+        else: 
+            self.tail.next = new_node 
+            new_node.prev = self.tail 
+            self.tail - new_node 
+        self.length += 1 
+        return True 
+    
+    def pop(self): 
+        if self.length == 0 : 
+            return None 
+        else : 
+            temp = self.tail 
+            self.tail = self.tail.prev 
+            self.tail.next = None 
+            temp.prev = None
+            return temp 
+        return None 
+    
+    
+            
